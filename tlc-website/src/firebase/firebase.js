d40f7d630ebda01c743b38a7a8656c36e5dce32f
Zhi Wei <jayzw96@gmail.com>
2025-01-10 01:21:45 +0800

    LFS

--- tlc-website/src/firebase/firebase.js
+++ tlc-website/src/firebase/firebase.js
@@ -1,12 +1,12 @@
-// Import the functions you need from the SDKs you need
+// Import the functions need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
+// Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAsK-n9-J3vzVEVWcBMRQ6PLtGhbA1CniI",
