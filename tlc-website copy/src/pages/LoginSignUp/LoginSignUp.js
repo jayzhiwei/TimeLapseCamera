24428edb3a0db54e35bfacd3e09b2ccbd99f83b1
jayzw96 <jayzw96@gmail.com>
2025-01-18 14:34:43 +0800

    UI v0.2
    Move login and sign up page

---
+++ tlc-website copy/src/pages/LoginSignUp/LoginSignUp.js
@@ -0,0 +1,8 @@
+import { signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
+import { auth, storage } from '../../firebase/firebase';
+
+function LoginSignUp(){
+    
+}
+
+export default LoginSignUp;
